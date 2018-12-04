466765cf6a28a5d2df3a7139bb9640136765a1ba
wassan128 <wassan.taiko.master@gmail.com>
2018-12-04 17:40:04 +0900

    [add] first repl

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"github.com/wassan128/monkey/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Fell free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
